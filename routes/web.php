7a2350b534274e5568e9b687d792e468df28c9a7
restumi <maulanaismael5@gmail.com>
2026-04-24 10:13:50 +0700

    feat: install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth')->group(function () {
+
+    Route::middleware('verified')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
+
+        Route::get('/chat', function () {
+            return view('chat');
+        })->name('chat');
+    });
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
