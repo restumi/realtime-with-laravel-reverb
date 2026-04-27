047f500f07a2d1ecb903a1608f4037aefc82b44f
restumi <maulanaismael5@gmail.com>
2026-04-27 10:45:38 +0700

    fix: chat chat realtime

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -2,7 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -13,22 +13,14 @@ class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
     public function __construct(
         public $message
     ){}
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, Channel>
-     */
     public function broadcastOn()
     {
         Log::info($this->message);
-        return new Channel('chat-channel');
+        return new PresenceChannel('chat-channel');
     }
 
     public function broadcastAs()
@@ -42,7 +34,6 @@ class MessageSent implements ShouldBroadcastNow
             'id' => $this->message->id,
             'user_id' => $this->message->user_id,
             'user_name' => $this->message->user->name ?? 'Guest',
-            'user_avatar' => $this->message->user?->profile_photo_url,
             'message' => $this->message->message,
             'created_at' => $this->message->created_at->diffForHumans(),
         ];
